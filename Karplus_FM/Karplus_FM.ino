58da6dd3d3376fec78a5e9eb71c99e7aa3a6d430
jlswbs <jlswbs@users.noreply.github.com>
2025-11-09 10:39:22 +0100

    Add files via upload

---
+++ Karplus_FM/Karplus_FM.ino
@@ -0,0 +1,176 @@
+// Karplus-FM modulated engine //
+
+#include "hardware/structs/rosc.h"
+#include "PicoSPI.h"
+
+#define MP3_CLK   2
+#define MP3_MOSI  3
+#define MP3_MISO  4
+#define MP3_CS    5
+#define MP3_XDCS  17
+#define MP3_DREQ  26
+#define MP3_RST   13
+
+#define SAMPLE_RATE 22050
+#define MAX_DELAY   1024
+#define BPM         120
+
+float freq1 = 220.0f;
+float freq2 = 1000.0f;
+float mod_index = 0.45f;
+float cutoff = 0.999f;
+float feedback = 0.95f;
+
+int delay_len = 0;
+int ks_index = 0;
+float prev_sample = 0.0;
+float ks_index_f = 0.0f;
+unsigned long n_samples = 0;
+float phase = 0.0;
+float ks_buf[MAX_DELAY];
+
+static inline uint32_t read_from_rosc() {
+  uint32_t random = 0;
+  volatile uint32_t *rnd_reg = (uint32_t *)(ROSC_BASE + ROSC_RANDOMBIT_OFFSET);
+  for (int k = 0; k < 32; k++) {
+    uint32_t bit = 0;
+    while (1) {
+      bit = (*rnd_reg) & 1;
+      if (bit != ((*rnd_reg) & 1)) break;
+    }
+    random = (random << 1) | bit;
+  }
+  return random;
+}
+
+void seed_random_safe() {
+  uint32_t seed = 0;
+  for (int i = 0; i < 8; i++) {
+    seed ^= read_from_rosc();
+    delayMicroseconds(100);
+  }
+  srand(seed);
+  randomSeed(seed);
+}
+
+const uint8_t wav_header[44] = {
+  0x52,0x49,0x46,0x46,0xFF,0xFF,0xFF,0xFF,0x57,0x41,0x56,0x45,
+  0x66,0x6d,0x74,0x20,0x10,0x00,0x00,0x00,0x01,0x00,0x02,0x00,
+  0x22,0x56,0x00,0x00,0x44,0xac,0x00,0x00,0x02,0x00,0x08,0x00,
+  0x64,0x61,0x74,0x61,0xFF,0xFF,0xFF,0xFF
+};
+
+void WriteReg16(unsigned char address, unsigned int databyte) {
+    while(!digitalRead(MP3_DREQ));
+    digitalWrite(MP3_CS, LOW);
+    PicoSPI0.transfer(0x02);
+    PicoSPI0.transfer(address);
+    PicoSPI0.transfer((databyte >> 8) & 0xFF);
+    PicoSPI0.transfer(databyte & 0xFF);
+    while(!digitalRead(MP3_DREQ));
+    digitalWrite(MP3_CS, HIGH);
+}
+
+void load_header() {
+    for (int i=0; i<44; i++) {
+        while(!digitalRead(MP3_DREQ)){ }
+        digitalWrite(MP3_XDCS, LOW);
+        PicoSPI0.transfer(wav_header[i]);
+        digitalWrite(MP3_XDCS, HIGH);
+    }
+}
+
+void init_karplus_fm(float f1, float f2, float mod_idx) {
+    freq1 = f1;
+    freq2 = f2;
+    mod_index = mod_idx;
+
+    delay_len = int(SAMPLE_RATE / freq1);
+    if (delay_len > MAX_DELAY) delay_len = MAX_DELAY;
+
+    for (int i = 0; i < delay_len; i++) {
+        ks_buf[i] = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
+    }
+
+    ks_index = 0;
+    prev_sample = 0.0;
+    phase = 0.0;
+    n_samples = 0;
+}
+
+float karplus_fm_sample() {
+
+    float phase_inc = freq1 / SAMPLE_RATE * (1.0f + mod_index * sin(2.0f * 3.14159265f * freq2 * n_samples / SAMPLE_RATE));
+
+    int i0 = int(ks_index_f);
+    int i1 = (i0 + 1) % delay_len;
+    float frac = ks_index_f - i0;
+    float cur = ks_buf[i0] * (1 - frac) + ks_buf[i1] * frac;
+
+    float new_sample = cutoff * (cur + prev_sample) / 2.0f;
+    ks_buf[i0] = new_sample * feedback;
+    prev_sample = new_sample;
+
+    ks_index_f += phase_inc * delay_len;
+    if (ks_index_f >= delay_len) ks_index_f -= delay_len;
+
+    n_samples++;
+
+    return cur;
+}
+
+
+void setup() {
+
+    seed_random_safe();
+    
+    pinMode(MP3_DREQ, INPUT);
+    pinMode(MP3_CS, OUTPUT);
+    pinMode(MP3_XDCS, OUTPUT);
+    pinMode(MP3_RST, OUTPUT);
+
+    PicoSPI0.configure(MP3_CLK, MP3_MOSI, MP3_MISO, MP3_CS, 4000000, 0, true);
+    PicoSPI0.transfer(0xFF);
+    digitalWrite(MP3_RST, HIGH);
+    digitalWrite(MP3_CS, HIGH);
+    digitalWrite(MP3_XDCS, HIGH);
+
+    load_header();
+    WriteReg16(0x03, 0x6000);
+    WriteReg16(0x0B, 0x4F4F);
+
+    PicoSPI0.configure(MP3_CLK, MP3_MOSI, MP3_MISO, MP3_CS, 16000000, 0, true);
+    PicoSPI0.transfer(0xFF);
+
+}
+
+void loop() {
+
+    while(!digitalRead(MP3_DREQ)) {}
+
+    uint8_t sample = 128 + 127.0f * karplus_fm_sample();
+
+    digitalWrite(MP3_XDCS, LOW);
+    PicoSPI0.transfer(sample);
+    PicoSPI0.transfer(sample);
+    digitalWrite(MP3_XDCS, HIGH);
+
+}
+
+void setup1() {
+
+  seed_random_safe();
+
+}
+
+void loop1() {
+
+    freq1 = random(100, 880);
+    freq2 = random(100, 3000);
+
+    init_karplus_fm(freq1, freq2, mod_index);
+
+    int tempo = 60000 / BPM;
+    delay(tempo / 3);
+
+}
